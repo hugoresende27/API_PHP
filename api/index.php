a4b1758ebbf09b919c67cb6d28a84a463eec520c
hugoresende27 <Hugoresende27@gmail.com>
2021-10-24 11:51:59 +0100

    dom

---
+++ api/index.php
@@ -0,0 +1,35 @@
+<?php
+
+$data = [];
+
+//request
+if (isset($_GET['option']))     //obrigatório var 'option' e 'status'
+{
+    switch($_GET['option'])
+    {
+        case 'status':
+            $data['status'] = 'SUCCESS';
+            $data['data']  = 'API running OK!';
+            break;
+
+        default :
+            $data['status'] = 'ERROR';
+            break;
+    }
+
+}
+else
+{
+    $data['status'] = 'ERROR';
+}
+
+//emitir a resposta da API
+response($data);
+
+//construção da response
+
+function response($data_response)
+{
+    header("Content-Type:application/json");  //resposta em formato json
+    echo json_encode($data_response);
+}
